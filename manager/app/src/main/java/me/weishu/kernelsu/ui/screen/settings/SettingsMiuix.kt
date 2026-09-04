c4a8654d8da3148630e3813b0515d234f49b4def
YuKongA <70465933+YuKongA@users.noreply.github.com>
2026-09-04 18:03:26 +0800

    manager: redesign home page layout
    Signed-off-by: Guxin12 <2452148391@qq.com>

--- manager/app/src/main/java/me/weishu/kernelsu/ui/screen/settings/SettingsMiuix.kt
+++ manager/app/src/main/java/me/weishu/kernelsu/ui/screen/settings/SettingsMiuix.kt
@@ -15,23 +15,25 @@ import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.Article
+import androidx.compose.material.icons.automirrored.rounded.Rule
 import androidx.compose.material.icons.rounded.Adb
+import androidx.compose.material.icons.rounded.AdminPanelSettings
 import androidx.compose.material.icons.rounded.BugReport
 import androidx.compose.material.icons.rounded.Code
 import androidx.compose.material.icons.rounded.ContactPage
 import androidx.compose.material.icons.rounded.Dashboard
 import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Description
 import androidx.compose.material.icons.rounded.DeveloperMode
-import androidx.compose.material.icons.rounded.ElectricalServices
-import androidx.compose.material.icons.rounded.Fence
-import androidx.compose.material.icons.rounded.FolderDelete
+import androidx.compose.material.icons.rounded.DisplaySettings
+import androidx.compose.material.icons.rounded.FlashOn
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material.icons.rounded.LayersClear
 import androidx.compose.material.icons.rounded.Palette
-import androidx.compose.material.icons.rounded.Policy
-import androidx.compose.material.icons.rounded.RemoveCircle
-import androidx.compose.material.icons.rounded.RemoveModerator
 import androidx.compose.material.icons.rounded.RestartAlt
-import androidx.compose.material.icons.rounded.Update
-import androidx.compose.material.icons.rounded.UploadFile
+import androidx.compose.material.icons.rounded.Security
+import androidx.compose.material.icons.rounded.SystemUpdate
+import androidx.compose.material.icons.rounded.SystemUpdateAlt
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -118,7 +120,7 @@ fun SettingPagerMiuix(
                             summary = stringResource(id = R.string.settings_check_update_summary),
                             startAction = {
                                 Icon(
-                                    Icons.Rounded.Update,
+                                    Icons.Rounded.SystemUpdate,
                                     modifier = Modifier.padding(end = 6.dp),
                                     contentDescription = stringResource(id = R.string.settings_check_update),
                                     tint = colorScheme.onBackground
@@ -133,7 +135,7 @@ fun SettingPagerMiuix(
                                 summary = stringResource(id = R.string.settings_check_update_summary),
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.UploadFile,
+                                        Icons.Rounded.SystemUpdateAlt,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = stringResource(id = R.string.settings_check_update),
                                         tint = colorScheme.onBackground
@@ -156,7 +158,7 @@ fun SettingPagerMiuix(
                             items = UiMode.entries.map { it.name },
                             startAction = {
                                 Icon(
-                                    Icons.Rounded.Dashboard,
+                                    Icons.Rounded.DisplaySettings,
                                     modifier = Modifier.padding(end = 6.dp),
                                     contentDescription = stringResource(id = R.string.settings_ui_mode),
                                     tint = colorScheme.onBackground
@@ -192,7 +194,7 @@ fun SettingPagerMiuix(
                                 summary = stringResource(id = R.string.settings_profile_template_summary),
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.Fence,
+                                        Icons.Rounded.Description,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = profileTemplate,
                                         tint = colorScheme.onBackground
@@ -251,7 +253,7 @@ fun SettingPagerMiuix(
                                 items = suCompatModeItems,
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.RemoveModerator,
+                                        Icons.Rounded.AdminPanelSettings,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = stringResource(id = R.string.settings_sucompat),
                                         tint = colorScheme.onBackground
@@ -272,7 +274,7 @@ fun SettingPagerMiuix(
                                 summary = umountSummary,
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.RemoveCircle,
+                                        Icons.Rounded.LayersClear,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = stringResource(id = R.string.settings_kernel_umount),
                                         tint = colorScheme.onBackground
@@ -293,7 +295,7 @@ fun SettingPagerMiuix(
                                 summary = selinuxHideSummary,
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.Policy,
+                                        Icons.Rounded.Security,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = stringResource(id = R.string.settings_selinux_hide),
                                         tint = colorScheme.onBackground
@@ -372,7 +374,7 @@ fun SettingPagerMiuix(
                                 summary = stringResource(id = R.string.settings_umount_modules_default_summary),
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.FolderDelete,
+                                        Icons.AutoMirrored.Rounded.Rule,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = stringResource(id = R.string.settings_umount_modules_default),
                                         tint = colorScheme.onBackground
@@ -401,7 +403,7 @@ fun SettingPagerMiuix(
                                 summary = stringResource(id = R.string.settings_auto_jailbreak_summary),
                                 startAction = {
                                     Icon(
-                                        Icons.Rounded.ElectricalServices,
+                                        Icons.Rounded.FlashOn,
                                         modifier = Modifier.padding(end = 6.dp),
                                         contentDescription = stringResource(id = R.string.settings_auto_jailbreak),
                                         tint = if (uiState.isLateLoadMode) colorScheme.onBackground else colorScheme.disabledOnSecondaryVariant
@@ -468,7 +470,7 @@ fun SettingPagerMiuix(
                             title = about,
                             startAction = {
                                 Icon(
-                                    Icons.Rounded.ContactPage,
+                                    Icons.Rounded.Info,
                                     modifier = Modifier.padding(end = 6.dp),
                                     contentDescription = about,
                                     tint = colorScheme.onBackground
